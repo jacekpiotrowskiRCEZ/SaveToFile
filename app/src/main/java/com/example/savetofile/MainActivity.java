8fd4effa0e7aec499018b8a3bdffdc6e97825828
jacekpiotrowskiRCEZ <76050734+jacekpiotrowskiRCEZ@users.noreply.github.com>
2022-11-13 11:46:47 +0100

    Initial commit

---
+++ app/src/main/java/com/example/savetofile/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.savetofile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         editText = (EditText) findViewById(R.id.editText);
+    }
+    public void onClickWczytaj(View view){
+        try
+        {
+            FileInputStream fIn = openFileInput("textfile.txt");
+            InputStreamReader isr = new InputStreamReader(fIn);
+            BufferedReader reader = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String linia;
+            while ((linia = reader.readLine()) != null) {
+                sb.append(linia).append("\n");
+            }
+            reader.close();
+
+
+            editText.setText(sb.toString());
+            Toast.makeText(this, "Plik został wczytany!",       Toast.LENGTH_SHORT).show();
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    public void onClickZapisz(View view) {
+        String tekst = editText.getText().toString();
+        try {
+           // FileOutputStream fOut = openFileOutput("textfile.txt",    MODE_PRIVATE);
+            FileOutputStream fOut = openFileOutput("textfile.txt",MODE_APPEND);
+            OutputStreamWriter osw = new OutputStreamWriter(fOut);
+            osw.write(tekst);
+            osw.close();
+            Toast.makeText(this, "Plik został zapisany!",
+                    Toast.LENGTH_SHORT).show();
+            editText.setText("");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
